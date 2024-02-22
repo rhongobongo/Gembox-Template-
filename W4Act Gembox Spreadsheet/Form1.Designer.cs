09edcd107bce7b8aaf122db33006a945e48ad25a
rhongobongo <jeffgabriel.leung@cit.edu>
2024-02-22 20:28:40 +0800

    Add project files.

---
+++ W4Act Gembox Spreadsheet/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace W4Act_Gembox_Spreadsheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvClassList = new DataGridView();
+            btnLoad = new Button();
+            btnSave = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvClassList).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvClassList
+            // 
+            dgvClassList.AllowUserToAddRows = false;
+            dgvClassList.AllowUserToDeleteRows = false;
+            dgvClassList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvClassList.Location = new Point(12, 12);
+            dgvClassList.Name = "dgvClassList";
+            dgvClassList.ReadOnly = true;
+            dgvClassList.RowTemplate.Height = 25;
+            dgvClassList.Size = new Size(776, 378);
+            dgvClassList.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(571, 396);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(103, 42);
+            btnLoad.TabIndex = 1;
+            btnLoad.Text = "Open File";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(680, 396);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(108, 42);
+            btnSave.TabIndex = 2;
+            btnSave.Text = "Save File";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSave);
+            Controls.Add(btnLoad);
+            Controls.Add(dgvClassList);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvClassList).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvClassList;
+        private Button btnLoad;
+        private Button btnSave;
+    }
+}
---
+++ W4Act Gembox Spreadsheet/Form1.cs
@@ -0,0 +1,93 @@
+using GemBox.Spreadsheet;
+using GemBox.Spreadsheet.WinFormsUtilities;
+using System.Diagnostics;
+
+namespace W4Act_Gembox_Spreadsheet
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter =
+            "XLS files(*.xls, *.xlt)|*.xls; *.xlt|" +
+            "XLSX files(*.xlsx, *.xlsm, *.xltx, *.xltm)|*.xlsx; *.xlsm; *.xltx; *.xltm|" +
+            "ODS files (*.ods, *.ots)|*.ods; *.ots|" +
+            "CSV files (*.csv, *.tsv)|*.csv; *.tsv|" +
+            "HTML files (*.html, *.htm)|*.html; *.htm";
+
+            openFileDialog.FilterIndex = 2;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var workbook = ExcelFile.Load(openFileDialog.FileName);
+                var worksheet = workbook.Worksheets.ActiveWorksheet;
+
+                DataGridViewConverter.ExportToDataGridView(
+                worksheet,
+                this.dgvClassList,
+                new ExportToDataGridViewOptions() { ColumnHeaders = true }
+            );
+
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter =
+                "XLS (*.xls)|*.xls|" +
+                "XLT (*.xlt)|*.xlt|" +
+                "XLSX (*.xlsx)|*.xlsx|" +
+                "XLSM (*.xlsm)|*.xlsm|" +
+                "XLTX (*.xltx)|*.xltx|" +
+                "XLTM (*.xltm)|*.xltm|" +
+                "ODS (*.ods)|*.ods|" +
+                "OTS (*.ots)|*.ots|" +
+                "CSV (*.csv)|*.csv|" +
+                "TSV (*.tsv)|*.tsv|" +
+                "HTML (*.html)|*.html|" +
+                "MHTML (*.mhtml)|*.mhtml|" +
+                "PDF (*.pdf)|*.pdf|" +
+                "XPS (*.xps)|*.xps|" +
+                "BMP (*.bmp)|*.bmp|" +
+                "GIF (*.gif)|*.gif|" +
+                "JPEG (*.jpg)|*.jpg|" +
+                "PNG (*.png)|*.png|" +
+                "TIFF (*.tif)|*.tif|" +
+                "WMP (*.wdp)|*.wdp";
+
+            saveFileDialog.FilterIndex = 3;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var workbook = new ExcelFile();
+                var worksheet = workbook.Worksheets.Add("Sheet1");
+
+                DataGridViewConverter.ImportFromDataGridView(
+                    worksheet,
+                    this.dgvClassList,
+                    new ImportFromDataGridViewOptions() { ColumnHeaders = true }
+                    );
+                workbook.Save(saveFileDialog.FileName);
+            }
+
+        }
+
+
+    }
+}
